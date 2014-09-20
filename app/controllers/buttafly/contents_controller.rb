e89198a4fc97b0200382adb29cfb3af2675c8889
fdschoeneman <fred.schoeneman@gmail.com>
2014-09-19 18:16:54 -0700

    added mappings and contents controllers

---
+++ app/controllers/buttafly/contents_controller.rb
@@ -0,0 +1,26 @@
+require_dependency "buttafly/application_controller"
+
+module Buttafly
+  class ContentsController < ApplicationController
+    def new
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    def index
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,30 @@
 Buttafly::Engine.routes.draw do
+  get 'contents/new'
+
+  get 'contents/show'
+
+  get 'contents/edit'
+
+  get 'contents/create'
+
+  get 'contents/update'
+
+  get 'contents/destroy'
+
+  get 'contents/index'
+
+  get 'mapping/new'
+
+  get 'mapping/show'
+
+  get 'mapping/edit'
+
+  get 'mapping/index'
+
+  get 'mapping/update'
+
+  get 'mapping/destroy'
+
+  get 'mapping/create'
+
 end
